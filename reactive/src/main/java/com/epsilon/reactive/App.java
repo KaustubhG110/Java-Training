1e07b9d054bc6876f5e4aa3b271b9909f0546d24
Kaustubh Gharat <kaustubhgharat110@gmail.com>
2021-09-22 12:06:52 +0530

    Publisher-Subscriber

--- reactive/src/main/java/com/epsilon/reactive/App.java
+++ reactive/src/main/java/com/epsilon/reactive/App.java
@@ -1,5 +1,7 @@
 package com.epsilon.reactive;
 
+import reactor.core.publisher.Flux;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Flux<Integer> publisher = Flux.just(1,2,3,4,5,6);
+        
+        publisher.map(x -> x*x).filter(x -> x%2 == 0).subscribe(System.out::println);
+        //publisher.log().subscribe(System.out::println);
     }
 }
